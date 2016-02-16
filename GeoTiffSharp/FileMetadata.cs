b4c90ca42b0829be4d90ba0c73af8bd36a02248f
Stefan Gordon <stefan@stefangordon.com>
2016-02-16 12:32:37 -0800

    renames

---
+++ GeoTiffSharp/FileMetadata.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoTiffSharp
+{
+    public class FileMetadata
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public double PixelScaleX { get; set; }
+        public double PixelScaleY { get; set; }
+        public double OriginLatitude { get; set; }
+        public double OriginLongitude { get; set; }
+        public int BitsPerSample { get; set; }
+        public string WorldUnits { get; set; }
+    }
+}
