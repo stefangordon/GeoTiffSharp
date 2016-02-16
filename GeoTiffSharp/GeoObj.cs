cc80abbaef35db8d2a7847fca6ce55c5327dd3ec
Andre Price <obsoleted@users.noreply.github.com>
2016-02-16 13:17:10 -0800

    Adding start of obj parsing

---
+++ GeoTiffSharp/GeoObj.cs
@@ -0,0 +1,36 @@
+﻿using ObjParser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoTiffSharp
+{
+    class GeoObj
+    {
+        static readonly int BITS_PER_SAMPLE = 16;
+
+        public static FileMetadata ParseMetadata(string filename)
+        {
+            FileMetadata metadata = new FileMetadata();
+
+            Obj obj = new Obj();
+            obj.LoadObj(filename);
+
+            metadata.BitsPerSample = BITS_PER_SAMPLE;
+
+            metadata.Height = (int)Math.Ceiling(obj.Size.ZMax - obj.Size.ZMin);
+            metadata.Width = (int)Math.Ceiling(obj.Size.XMax - obj.Size.XMin);
+
+            metadata.OriginLatitude = obj.Size.XMin;
+            metadata.OriginLongitude = obj.Size.ZMax;
+
+            metadata.WorldUnits = "meters";
+
+
+            return metadata;
+
+        }
+    }
+}
